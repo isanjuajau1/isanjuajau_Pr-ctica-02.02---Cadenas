5e9368c398385df9c44a8e7e81d7df52d0d33f37
isanjuajau1 <isanjuajau1@educacion.navarra.es>
2024-10-18 17:57:19 +0200

    EJERCICIO 10

--- EJERCICIO 10.py
+++ EJERCICIO 10.py
@@ -1,3 +1,3 @@
 # Escribir un programa que pregunte por consola por los productos de una cesta de la compra, separados por comas, y muestre por pantalla cada uno de los productos en una línea distinta.
-cesta = input('Introduce los productos de la cesta de la compra separados por comas: ')
+cesta = input('productos cesta de la compra separados por comas: ')
 print('\n'.join(cesta.split(',')))
